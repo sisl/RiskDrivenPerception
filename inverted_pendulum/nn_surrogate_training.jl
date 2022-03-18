c5eae9f73bef7a38f71f744c85e92ffe6541814f
Sydney Katz <sydney.m.katz@wustl.edu>
2022-03-18 02:08:31 -0700

    full process testing

--- inverted_pendulum/nn_surrogate_training.jl
+++ inverted_pendulum/nn_surrogate_training.jl
@@ -2,14 +2,25 @@ using POMDPs, POMDPGym, Crux, Flux, Distributions, Plots, BSON, GridInterpolatio
 include("../src/risk_solvers.jl")
 include("../inverted_pendulum/controllers/rule_based.jl")
 include("problem_setup.jl")
+include("siren.jl")
 
 s0 = [0.0, 0, 0.0]
 # Load the environmetn and policy
 env = InvertedPendulumMDP(λcost = 0.1f0, failure_thresh = π,
-    θ0 = Uniform(s0[2], s0[2] + 1e-6),
-    ω0 = Uniform(s0[3], s0[3] + 1e-6))
+        θ0 = Uniform(s0[2], s0[2] + 1e-6),
+        ω0 = Uniform(s0[3], s0[3] + 1e-6))
 nn_policy = BSON.load("inverted_pendulum/controllers/policy.bson")[:policy]
-simple_policy = FunPolicy(f)
+simple_policy = FunPolicy(continuous_rule(0, 2.0, -1))
+
+# k2a = 0:1:10
+# k3a = -10:1:10
+
+# rmap = reshape([undiscounted_return(Sampler(env, FunPolicy(continuous_rule(.0, 2.0, -1))), Neps = 1) for k2 in k2a for k3 in k3a], length(k2a), :)
+
+# heatmap(k2a, k3a, rmap)
+
+# undiscounted_return(Sampler(env, FunPolicy(continuous_rule(0.0, 2.0, -1))), Neps = 10)
+# Crux.gif(env, FunPolicy(continuous_rule(0.0, 10.0, -10)), "test.gif")
 
 rmdp, px, θs, ωs, s_grid, 𝒮, s2pt, cost_points, ϵ1s, ϵ2s, ϵ_grid = rmdp_pendulum_setup(env, simple_policy)
 Qw = solve_cvar_fixed_particle(rmdp, px.distribution, s_grid, 𝒮, s2pt, cost_points);
@@ -22,46 +33,58 @@ CVaR(s, ϵ, α) = CVaR(s2pt([0.0, s...]), ϵ, s_grid, ϵ_grid, Qw, cost_points; 
 # Set the desired α
 α = 0.0
 
-# Generate training data
-big_grid = RectangleGrid(θs, ωs, ϵ1s, ϵ2s)
+θs_small = θs[18:32]
+ωs_small = ωs[18:32]
 
-normalizers =  Dict()
+# Generate training data
+big_grid = RectangleGrid(θs_small, ωs_small, ϵ1s, ϵ2s)
+
+normalizers = Dict()
 X = zeros(4, length(big_grid))
 y = zeros(1, length(big_grid))
 for (i, (θ, ω, ϵ1, ϵ2)) in enumerate(big_grid)
     s = [θ, ω]
-    if !haskey(normalizers, s)
-        normalizers[s] = ECVaR(s, α)
-    end
-        
+    # if !haskey(normalizers, s)
+    #     normalizers[s] = ECVaR(s, α)
+    # end
+
     (i % 10000) == 0 && println("i=$i")
     X[:, i] .= (θ, ω, ϵ1, ϵ2)
-    y[i] = normalized_CVaR(s, [ϵ1, ϵ2], α, normalizer=normalizers[s])
+    # y[i] = normalized_CVaR(s, [ϵ1, ϵ2], α, normalizer = normalizers[s])
+    y[i] = CVaR(s, [ϵ1, ϵ2], α)
 end
 X = X |> gpu
 y = y |> gpu
-data = Flux.DataLoader((X,y), batchsize=length(y), shuffle=true)
+data = Flux.DataLoader((X, y), batchsize = length(y), shuffle = true)
 
 
 # Create the model and optimizer
-model = Chain(Dense(4, 128, relu), Dense(128, 64, relu), Dense(64, 1, softplus)) |> gpu
+# model = Chain(SirenDense(4, 256, isfirst = true),
+#     SirenDense(256, 256),
+#     SirenDense(256, 256),
+#     # SirenDense(256, 256),
+#     Dense(256, 1, softplus)) |> gpu
+model = Chain(Dense(4, 128, relu),
+    Dense(128, 64, relu),
+    Dense(64, 1)) |> gpu
 θ = Flux.params(model)
 opt = ADAM(1e-3)
 
 # Setup the training
-loss(x,y) = Flux.mse(model(x), y)
+loss(x, y) = Flux.mse(model(x), y)
 evalcb() = println("train loss: ", loss(X, y))
-throttlecb = Flux.throttle(evalcb, 1)
+throttlecb = Flux.throttle(evalcb, 0.1)
 
 # Train
-Flux.@epochs 100 Flux.train!(loss, θ, data, opt, cb=throttlecb)
+Flux.@epochs 1000 Flux.train!(loss, θ, data, opt, cb = throttlecb)
 
+model = model |> cpu
+BSON.@save "inverted_pendulum/risk_networks/risk_net_k1_0_unnorm.bson" model
 
-heatmap(θs, ωs, (x, y) -> cpu(model)([x, y, 0, 0])[1], clims=(0,π))
-heatmap(θs, ωs, (x, y) -> normalized_CVaR([x, y], [0, 0], 0.0), clims=(0,π))
+heatmap(θs_small, ωs_small, (x, y) -> model([x, y, 0, 0])[1], clims = (0, π))
+heatmap(θs_small, ωs_small, (x, y) -> CVaR([x, y], [0, 0], 0.0), clims = (0, π))
 
-heatmap(ϵ1s, ϵ2s, (x, y) -> cpu(model)([0.2, 0, x, y])[1], clims=(0,π))
-heatmap(ϵ1s, ϵ2s, (x, y) -> normalized_CVaR([0.2, 0], [x, y], 0.0), clims=(0,π))
+heatmap(ϵ1s, ϵ2s, (x, y) -> model([-0.2, 0, x, y])[1], clims = (0, π))
+heatmap(ϵ1s, ϵ2s, (x, y) -> CVaR([-0.2, 0], [x, y], 0.0), clims = (0, π))
 
-y
 
--- inverted_pendulum/training.jl
+++ inverted_pendulum/training.jl
@@ -1,85 +1,128 @@
 using POMDPs, POMDPGym, Crux, Flux, Colors, Distributions, Plots, BSON, Printf
-
+include("../inverted_pendulum/controllers/rule_based.jl")
 ## Train state-based controller
 
 # Generate the environment
-env = InvertedPendulumMDP(λcost=0.1f0)
+env = InvertedPendulumMDP(λcost = 0.1f0, failure_thresh = π)
+simple_policy = FunPolicy(continuous_rule(0.0, 2.0, -1))
 
-# Define the networks we will use
-A() = DiscreteNetwork(Chain(Dense(2, 12, relu), Dense(12, 2)), env.actions)
-C() = ContinuousNetwork(Chain(Dense(2, 12, relu), Dense(12, 1)))
+# # Define the networks we will use
+# A() = DiscreteNetwork(Chain(Dense(2, 12, relu), Dense(12, 2)), env.actions)
+# C() = ContinuousNetwork(Chain(Dense(2, 12, relu), Dense(12, 1)))
 
-# Solve for a policy
-solver = PPO(π=ActorCritic(A(), C()), S=state_space(env), N=5000, ΔN=100)
-policy = solve(solver, env)
-BSON.@save "inverted_pendulum/controllers/policy.bson" policy
-# π_ppo = BSON.load("inverted_pendulum/controllers/policy.bson")[:policy]
+# # Solve for a policy
+# solver = PPO(π=ActorCritic(A(), C()), S=state_space(env), N=5000, ΔN=100)
+# policy = solve(solver, env)
+# BSON.@save "inverted_pendulum/controllers/policy.bson" policy
+# # π_ppo = BSON.load("inverted_pendulum/controllers/policy.bson")[:policy]
 
 
-# Create some visualizations
-Crux.gif(env, policy, "inverted_pendulum/figures/pendulum_control.gif")
-heatmap(-0.4:0.05:0.4, -1:0.05:1, (θ, ω) -> action(policy, [θ, ω])[1], title="Pendulum Control Policy", xlabel="θ", ylabel="ω")
-savefig("inverted_pendulum/figures/controller_policy.png")
+# # Create some visualizations
+# Crux.gif(env, policy, "inverted_pendulum/figures/pendulum_control.gif")
+# heatmap(-0.4:0.05:0.4, -1:0.05:1, (θ, ω) -> action(policy, [θ, ω])[1], title="Pendulum Control Policy", xlabel="θ", ylabel="ω")
+# savefig("inverted_pendulum/figures/controller_policy.png")
 
 
 ## Train the perception system
-obsfn = (s)->POMDPGym.simple_render_pendulum(s, show_prev=false)
+obsfn = (s) -> POMDPGym.simple_render_pendulum(s, show_prev = false)
 
 # Generate training images
 N = 1000
 y, X = zeros(Float32, 2, N), zeros(Float32, 18, 10, N)
-for i=1:N
-	y[:, i] = [rand(Uniform(-0.4, 0.4)), rand(Uniform(-2, 2))]
-	X[:, :, i] = obsfn(y[:,i])
+for i = 1:N
+    y[:, i] = [rand(Uniform(-0.4, 0.4)), rand(Uniform(-1.26, 1.26))]
+    X[:, :, i] = obsfn(y[:, i])
 end
 
 # Visualize some fo the training data
-ps = [plot(obsfn(y[:,i]), xaxis=false, xticks=false, yticks=false, titlefont=font(pointsize=8), title=@sprintf("θ=%0.2f, ω=%0.2f", y[1,i], y[2,i])) for i=1:10]
-plot(ps..., layout=(2,5))
-savefig("inverted_pendulum/figures/training_data.png")
+ps = [plot(obsfn(y[:, i]), xaxis = false, xticks = false, yticks = false, titlefont = font(pointsize = 8), title = @sprintf("θ=%0.2f, ω=%0.2f", y[1, i], y[2, i])) for i = 1:10]
+plot(ps..., layout = (2, 5))
+# savefig("inverted_pendulum/figures/training_data.png")
 
 # Prepare training data and model
-data = Flux.DataLoader((X,y), batchsize=128)
-model = Chain(flatten, Dense(180, 64, relu), Dense(64,64,relu), Dense(64, 2))
+data = Flux.DataLoader((X, y), batchsize = 128)
+model_reg = Chain(flatten, Dense(180, 64, relu), Dense(64, 64, relu), Dense(64, 2, tanh), x -> x .* [0.4, 1.26])
+model = Chain(flatten, Dense(180, 64, relu), Dense(64, 64, relu), Dense(64, 2, tanh), x -> x .* [0.4, 1.26])
 opt = ADAM(1e-3)
-loss(x,y) = Flux.Losses.mse(model(x), y)
+loss(x, y) = Flux.Losses.mse(model_reg(x), y)
+
+# Load in the risk network
+risk_function = BSON.load("inverted_pendulum/risk_networks/risk_net_k1_0_unnorm.bson")[:model]
+
+risk_function([0.5, 0, 0, 0])[1]
+
 function ρloss(x, y)
-	# ϵ = model(x) .- y
-	ϵ = zeros(4, size(y,2))
-	res = zeros(1, size(y, 2))
-	for i=1:size(y,2)
-		res[i] = foo(ϵ[:,i])
-	end
-	Flux.mean((ϵ .^ 2) .* res)
+    ŷ = model(x)
+    ϵ = y .- ŷ
+    ϵ_risk = risk_function([y; ϵ])
+    weighted_errors = ϵ .* ϵ_risk
+    return mean(weighted_errors .^ 2)
 end
 
+function ρ2loss(x, y)
+    ŷ = model(x)
+    ϵ = y .- ŷ
+    ϵ_risk = risk_function([y; ϵ])
+    return mean(ϵ_risk)
+end
+
+function ρ3loss(x, y)
+    ŷ = model(x)
+    ϵ = y .- ŷ
+    ϵ_risk = risk_function([y; ϵ])
+    return Flux.mse(ŷ, y) + mean(ϵ_risk)
+end
 
 # Train the model
-# Flux.@epochs 100 Flux.train!(loss, Flux.params(model), data, opt)
-Flux.@epochs 100 Flux.train!(ρloss, Flux.params(model), data, opt)
+evalcb() = println("train loss: ", ρ2loss(X, y))
+throttlecb = Flux.throttle(evalcb, 0.1)
+
+Flux.@epochs 1000 Flux.train!(loss, Flux.params(model_reg), data, opt, cb = throttlecb)
+opt = ADAM(1e-3)
+Flux.@epochs 1000 Flux.train!(ρ2loss, Flux.params(model), data, opt, cb = throttlecb)
 
 # Show the errors
 ŷ = model(X)
 
-scatter(y[2,:], ŷ[2,:], label = "ω", alpha=0.2, xlabel="Ground Truth", ylabel="Predicted", title="Perception Model Accuracy")
-scatter!(y[1,:], ŷ[1,:], label = "θ", alpha=0.2)
-savefig("inverted_pendulum/figures/perception_model_accuracy.png")
+scatter(y[2, :], ŷ[2, :], label = "ω", alpha = 0.2, xlabel = "Ground Truth", ylabel = "Predicted", title = "Perception Model Accuracy")
+scatter!(y[1, :], ŷ[1, :], label = "θ", alpha = 0.2)
+# savefig("inverted_pendulum/figures/perception_model_accuracy_nom.png")
+
+ŷ = model_reg(X)
+scatter(y[2, :], ŷ[2, :], label = "ω", alpha = 0.2, xlabel = "Ground Truth", ylabel = "Predicted", title = "Perception Model Accuracy")
+scatter!(y[1, :], ŷ[1, :], label = "θ", alpha = 0.2)
 
 
 ## Evaluate the combined system
 
 # Construct the image-based pendulum environment
-img_env = ImageInvertedPendulum(λcost=0.0f0, observation_fn=obsfn)
+img_env = ImageInvertedPendulum(λcost = 0.0f0, observation_fn = obsfn)
 
 # Construct the full policy by chaining the perception and state-based
-π_img = DiscreteNetwork(Chain((x) -> model(reshape(obsfn(x), 180,1)), (x) -> policy.A.network(x)), env.actions)
+π_img = ContinuousNetwork(Chain((x) -> model(reshape(x, 180, 1)), (x) -> [action(simple_policy, x)]), 1)
+π_img_reg = ContinuousNetwork(Chain((x) -> model_reg(reshape(x, 180, 1)), (x) -> [action(simple_policy, x)]), 1)
 
+simple_policy
 
 # Plot the resulting policy
-heatmap(-0.4:0.05:0.4, -1:0.05:1, (θ, ω) -> action(policy, model(reshape(obsfn([θ, ω]), 180,1)))[1], title="Image Control Policy", xlabel="θ", ylabel="ω")
-savefig("inverted_pendulum/figures/image_control_policy.png")
+heatmap(-0.4:0.05:0.4, -1:0.05:1, (θ, ω) -> action(simple_policy, model(reshape(Float32.(obsfn([θ, ω])), 180, 1))[:]), title = "Image Control Policy", xlabel = "θ", ylabel = "ω")
+heatmap(-0.4:0.05:0.4, -1:0.05:1, (θ, ω) -> action(simple_policy, model_reg(reshape(Float32.(obsfn([θ, ω])), 180, 1))[:]), title = "Image Control Policy", xlabel = "θ", ylabel = "ω")
+# savefig("inverted_pendulum/figures/image_control_policy_risk.png")
+
+
+s= rand(initialstate(img_env))
+o = observation(img_env,s)
+
+
+for i=1:10
+    s,o,r = gen(img_env, s, action(π_img, o)[1])
+    println(s)
+end 
+
 
 # Get the return and plot an episode
-undiscounted_return(Sampler(img_env, π_img), Neps=10)
+undiscounted_return(Sampler(img_env, π_img), Neps = 10)
+undiscounted_return(Sampler(img_env, π_img_reg), Neps = 10)
 Crux.gif(img_env, π_img, "inverted_pendulum/figures/img_pendulum.gif")
+Crux.gif(img_env, π_img_reg, "inverted_pendulum/figures/img_pendulum.gif")
 
