28b3b7399816db7d828478201805f5213f9827a2
craigiedon <craiginnes@gmail.com>
2022-04-22 17:05:18 +0100

    Gave the simulation code a go...but couldnt figure out julia in time...

--- collision_avoidance/simulate.jl
+++ collision_avoidance/simulate.jl
@@ -2,7 +2,7 @@ using POMDPs, POMDPGym, Crux, Flux, Distributions, GridInterpolations
 
 include("encounter_model/straight_line_model.jl")
 
-function simulate_encounter(enc::Encounter, policy)
+function simulate_encounter(enc::Encounter, policy)::Encounter
     """
     Inputs:
     - enc (Encounter): encounter to simulate (see encounter model file for details)
@@ -11,13 +11,43 @@ function simulate_encounter(enc::Encounter, policy)
     - sim_enc (Encounter): encounter object with simulated trajectories
     """
 
-    # TODO: fill this in!!
+    n_steps = length(enc.v0)
 
-    # At each time step
-    # Get current positions and speeds of ownship and intruder
-    # Use this to calculate mdp state
-    # Get action from policy
-    # Adjust ownship state accordingly
+    # Set initial state based on nominal trajectory start
+    # TODO: What is τ??? I couldn't figure this out...
+    s_current = (enc.z0[1] - enc.z1[1], enc.dh0[1], 0.0, 100)
+
+    z0 = zeros(n_steps)
+    z0[1] = s_current[1]
+
+    dh0 = zeros(n_steps)
+    dh0[1] = s_current[2]
+
+    # Step forward MDP using collision avoidance policy
+    for t in 2:n_steps-1
+        s_prev = s_current
+
+        # TODO: Couldn't figure out the api for this in time!
+        a = policy.action(s_current)
+        s_current = policy.transition(s_prev, a)
+
+        z0[t] = s_current[1]
+        dh0[t] = s_current[2]
+    end
+
+
+    return Encounter(
+        enc.x0,
+        enc.y0,
+        z0,
+        enc.v0,
+        dh0,
+        enc.x1,
+        enc.y1,
+        enc.z1,
+        enc.v1,
+        enc.dh1
+    )
 end
 
 simulate_encounters(encs, policy) = [simulate_encounter(enc, policy) for enc in encs]
@@ -45,6 +75,7 @@ dhs = range(-10, 10, length=21)
 policy = OptimalCollisionAvoidancePolicy(env, hs, dhs, τs)
 
 # Get 100 encounters
+nencs = 100
 encs = get_encounter_set(sampler, nencs)
 
 # Simulate them
